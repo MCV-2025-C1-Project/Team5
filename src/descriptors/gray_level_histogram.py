51b7a7ad20079b49a16f579e54b1a9b00ce16944
marinarosell <marrosmur@gmail.com>
2025-10-03 13:32:42 +0200

    feat: modify compute_histogram function to make the bin size adjustable with the values_per_bin parameter

--- src/descriptors/gray_level_histogram.py
+++ src/descriptors/gray_level_histogram.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import numpy as np
 
 
@@ -15,19 +16,36 @@ def convert_img_to_gray_scale(img: np.ndarray) -> np.ndarray:
     return img_gray
 
 
-def compute_histogram(img: np.ndarray, density: bool = True) -> tuple[np.ndarray, np.ndarray]:
-    """Compute the histogram of a grayscale image.
+def compute_histogram(
+    img: np.ndarray,
+        values_per_bin: int = 1,
+        density: bool = True
+) -> tuple[np.ndarray, np.ndarray]:
+    """Compute the histogram of a grayscale image with adjustable bin size.
 
     Args:
         img (np.ndarray): Input grayscale image.
-        density (bool, optional): If True, normalize the histogram so that the 
-            area under the histogram integrates to 1. If False, return raw counts. 
-            Defaults to True.
+        values_per_bin (int, optional): Number of consecutive intensity values 
+            grouped into each bin. For example, 1 creates 256 bins 
+            (0–255 individually), 2 creates 128 bins (0–1, 2–3, ...). 
+            Must be >= 1. Defaults to 1.
+        density (bool, optional): If True, normalize the histogram so the 
+            total area equals 1. If False, return raw counts. Defaults to True.
 
     Returns:
         tuple[np.ndarray, np.ndarray]: 
             - Histogram values (counts or normalized probabilities).
             - Bin edges corresponding to the histogram.
+
+    Raises:
+        ValueError: If `values_per_bin` is less than 1 or larger than 256.
     """
-    hist, bin_edges = np.histogram(img, 256, [0, 256], density=density)
+    if (values_per_bin < 1) or (values_per_bin > 256):
+        raise ValueError("values_per_bin must be >= 1 and <=256")
+
+    bins = math.ceil(256 / values_per_bin)
+    max_range = bins * values_per_bin
+
+    hist, bin_edges = np.histogram(
+        img, bins=bins, range=[0, max_range], density=density)
     return hist, bin_edges
