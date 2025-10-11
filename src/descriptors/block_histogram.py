5cde38a50b7436911c3219a02e2aefe51ee2021d
marinarosell <marrosmur@gmail.com>
2025-10-11 18:44:45 +0200

    fix: replace 'histrogram' everywhere

--- src/descriptors/block_histogram.py
+++ src/descriptors/block_histogram.py
@@ -67,7 +67,7 @@ def block_based_histogram_from_array(
     return np.concatenate(block_histograms), bins
 
 
-def block_based_histrogram(
+def block_based_histogram(
     img_path: str,
     compute_histogram_func,
     values_per_bin: int = 1,
